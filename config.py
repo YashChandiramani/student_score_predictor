084a62217899630dd9f7b811dc9090244e148217
YashChandiramani <chandiramaniyash@gmail.com>
2026-07-06 00:00:36 +0530

    Configure database for Aiven

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 MYSQL_USER = os.environ.get("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
 MYSQL_DB = os.environ.get("MYSQL_DB", "student_score_predictor")
--- db.py
+++ db.py
@@ -1,10 +1,12 @@
+import os
 import mysql.connector
-from config import *
 
 def get_connection():
     return mysql.connector.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        database=MYSQL_DB
+        host=os.environ.get("MYSQL_HOST"),
+        port=int(os.environ.get("MYSQL_PORT", 3306)),
+        user=os.environ.get("MYSQL_USER"),
+        password=os.environ.get("MYSQL_PASSWORD"),
+        database=os.environ.get("MYSQL_DB"),
+        ssl_disabled=False
     )
